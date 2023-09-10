89e76e5f72fb5d68cc3c41f95cc1368f9b37ad22
mathieuprog <5883963+mathieuprog@users.noreply.github.com>
2023-09-10 06:37:59 +0200

    Test rethrow error

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,12 @@
 import { expect, test } from 'vitest';
-import orThrow from '.';
+import { orThrow } from '.';
+
+class ValidationError extends Error {
+  constructor(message?: string) {
+    super(message);
+    this.name = 'ValidationError';
+  }
+}
 
 test('orThrow', () => {
   expect(() => orThrow(false)).toThrow();
@@ -9,4 +16,6 @@ test('orThrow', () => {
   expect(() => orThrow('')).toThrow();
   expect(() => orThrow(null, 'error message')).toThrow('error message');
   expect(orThrow(1)).toBe(1);
+
+  expect(() => orThrow(false, new ValidationError())).toThrow();
 });
