cef196bf79543c84cbdd6178bf61081ba2ee1ee9
mathieuprog <5883963+mathieuprog@users.noreply.github.com>
2023-09-10 06:37:36 +0200

    Add orThrowDeferred and allow to rethrow an error

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,40 @@
-export default function orThrow(value: unknown, message?: string) {
+function orThrow(value: unknown, messageOrError?: string | Error) {
   if (!value) {
-    throw new Error(message);
+    if (messageOrError === undefined) {
+      throw new Error();
+    }
+
+    if (typeof messageOrError === 'string') {
+      throw new Error(messageOrError);
+    }
+
+    if (messageOrError instanceof Error) {
+      throw messageOrError;
+    }
   }
 
   return value;
+}
+
+function orThrowDeferred(value: unknown, messageOrError?: string | Error) {
+  if (!value) {
+    if (messageOrError === undefined) {
+      setTimeout(() => { throw new Error() }, 0);
+    }
+
+    if (typeof messageOrError === 'string') {
+      setTimeout(() => { throw new Error(messageOrError) }, 0);
+    }
+
+    if (messageOrError instanceof Error) {
+      setTimeout(() => { throw messageOrError }, 0);
+    }
+  }
+
+  return value;
+}
+
+export {
+  orThrow,
+  orThrowDeferred
 }
